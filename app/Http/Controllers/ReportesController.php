b5b8c8d7526591a64ade0fd96c4630ac68f7d7a6
Skgrendel <bryan.andion@gmail.com>
2024-07-09 10:56:35 -0500

    coreeccion

--- app/Http/Controllers/ReportesController.php
+++ app/Http/Controllers/ReportesController.php
@@ -45,8 +45,6 @@ class ReportesController extends Controller
      */
     public function store(Request $request)
     {
-
-        dd($request->all());
         $id = Auth::user()->personal->id;
         $ServicesStore = $this->Processing;
         $ServicesStore->StoreReport($request, $id);
