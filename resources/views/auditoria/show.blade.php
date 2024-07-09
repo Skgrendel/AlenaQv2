ce79f5cda6ea085068a7a86b4dd9fc86d370cf02
Skgrendel <bryan.andion@gmail.com>
2024-07-09 16:26:10 -0500

    corecciones

--- resources/views/auditoria/show.blade.php
+++ resources/views/auditoria/show.blade.php
@@ -576,7 +576,7 @@
                 width: $(this).data('width') ? $(this).data('width') : $(this).hasClass('w-100') ? '100%' :
                     'style',
                 placeholder: $(this).data('placeholder'),
-                closeOnSelect: false,
+                closeOnSelect: false, 
             });
         });
     </script>
