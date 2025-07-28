d996e4775a3c6d33f706386035d8da984bcd9a2a
skgrendel <bryan.andion@gmail.com>
2025-07-28 17:09:00 -0500

    df

--- resources/views/agentes/edit.blade.php
+++ resources/views/agentes/edit.blade.php
@@ -142,7 +142,7 @@
                                 <select id="anomalia" class="form-control select2" name="anomalia[]" multiple>
                                     @foreach ($data['anomalias'] as $id => $nombre)
                                         <option
-                                            value="{{ $nombre }}"{{ $data['info']['reporte']['anomalias'] ? 'selected' : '' }}>{{ $nombre }}</option>
+                                            value="{{ $nombre }}"{{ $data['info']['reporte']['anomaliasid'] ? 'selected' : '' }}>{{ $nombre }}</option>
                                     @endforeach
                                 </select>
                             </div>
