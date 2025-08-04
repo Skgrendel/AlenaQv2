a1eb72188b25fcf5309752162b6797728bdc1fd3
skgrendel <bryan.andion@gmail.com>
2025-08-04 12:08:45 -0500

    estados

--- app/Livewire/AsignadosDatatable.php
+++ app/Livewire/AsignadosDatatable.php
@@ -55,7 +55,7 @@ class AsignadosDatatable extends DataTableComponent
         // Por ejemplo, si quieres filtrar por el ID del personal del usuario autenticado:
       return surtigas::query()
             ->where('surtigas.personals_id', $personalId)
-            ->whereIn('ciclo', ['1005']);
+            ->whereIn('estado', '1');
     }
 
     public function columns(): array
