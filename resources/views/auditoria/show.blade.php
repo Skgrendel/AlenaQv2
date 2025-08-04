e9b7cfd7f477586741f711ba5dd4ea5511f5195b
skgrendel <bryan.andion@gmail.com>
2025-08-04 15:22:35 -0500

    c

--- resources/views/auditoria/show.blade.php
+++ resources/views/auditoria/show.blade.php
@@ -575,7 +575,7 @@
                                     @csrf
                                     @method('PUT')
                                     <div class="row">
-                                        <div class="col-3">
+                                        <div class="col-6">
                                             <span class="form-check-label">Revisado por Balance y Control</span>
                                             <div class="form-check form-check-inline">
                                                 <input class="form-check-input" type="radio" id="inlineCheckbox1"
